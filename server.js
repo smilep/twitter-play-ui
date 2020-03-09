e730cb3d22bbef21c2684a8b57d714c108af942a
Smile <smile.pabby@gmail.com>
2020-03-09 13:37:51 +0530

    Fixed server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-// server.js
 const express = require('express');
+const path = require('path');
 const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
@@ -9,14 +13,21 @@ const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
+        ['https://', req.get('Host'), req.url].join('')
       );
     }
     next();
   }
 }
-// Instruct the app
-// to use the forceSSL
-// middleware
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Instruct the app to use the forceSSL middleware
 app.use(forceSSL());
-// ...
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 12080);
